1d3a773de79366471156be8d5c829966cd922429
ogyammers <jamiegormannavan@gmail.com>
2022-03-15 20:53:23 +0000

    test

--- test/getprice.sh
+++ test/getprice.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -q -O - https://bigcharts.marketwatch.com/quickchart/quickchart.asp?symb=$1 | tr -d '\015' | tr '\n' ' ' | sed 's/^.*Last/Last/' | sed 's|\<td.*|<td|' | sed 's/^.*<div>//' | sed 's|\div.*||' | sed 's|<.*||' 
-echo \
+wget -q -O - https://bigcharts.marketwatch.com/quickchart/quickchart.asp?symb=$1 | tr -d '\015' | tr '\n' ' ' | sed 's/^.*Last/Last/' | sed 's|\<td.*|<td|' | sed 's/^.*<div>//' | sed 's|\div.*||' | sed 's|<.*||' | tr -d ',,' 
+
